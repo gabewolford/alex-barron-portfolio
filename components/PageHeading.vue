674215a5fb3902e9944fcebd6641ad15a9f1e466
gabewolford <gabewolford@gmail.com>
2023-11-06 16:29:02 -0800

    page heading component complete

--- components/PageHeading.vue
+++ components/PageHeading.vue
@@ -11,4 +11,14 @@ defineProps({
   <h1 class="text-4xl lg:text-[56px] font-apercu-bold lg:leading-[72px]">
     {{ heading }}
   </h1>
+  <p class="my-8 lg:my-16">
+    Previously, I was managing the design team at Parsley Health.
+    <br />
+    <br />
+    Currently, I am designing a platform that enables digital health companies
+    to view, interpret, and manage their data with Morf Health.
+    <br />
+    <br />
+    I'm also open to future full-time or contract opportunities.
+  </p>
 </template>
