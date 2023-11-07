a8bb77f3362046830c3550f19d6486658526a613
gabewolford <gabewolford@gmail.com>
2023-11-06 16:28:53 -0800

    added bottom margin for footer height

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
 
 <template>
   <AppHeader />
-  <main class="grid grid-cols-2 lg:grid-cols-12 gap-8 mx-8">
+  <main
+    class="grid grid-cols-2 lg:grid-cols-12 gap-8 mx-8 mb-[258px] lg:mb-[90px]"
+  >
     <div class="col-span-full lg:col-start-3 lg:col-span-8">
       <slot />
     </div>
