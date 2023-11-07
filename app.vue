fd9326e6aaac63036d95302325263c91873865c5
gabewolford <gabewolford@gmail.com>
2023-11-07 10:57:46 -0800

    added title and icon

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+useHead({
+  title: "Alex Barron | Design Portfolio",
+  meta: [{ name: "Alex Barron", content: "Product Design" }],
+  link: [{ rel: "icon", type: "image/svg", href: "/images/logo.svg" }],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
