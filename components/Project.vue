b78fac09c48a43c682368b541293c2da01f4b07b
gabewolford <gabewolford@gmail.com>
2023-11-07 11:29:50 -0800

    fixed coming soon conditional

--- components/Project.vue
+++ components/Project.vue
@@ -16,6 +16,8 @@ defineProps({
     type: String,
     required: true,
   },
+  isComingSoon: String,
+  required: true,
 });
 </script>
 
@@ -23,15 +25,23 @@ defineProps({
   <div
     class="grid grid-cols-2 lg:grid-cols-8 gap-4 lg:gap-8 border-t border-[#D7D4DB] py-4 lg:py-8 lg:text-2xl"
   >
-    <h3 class="col-start-1 text-[#9790A0]">{{ year }}</h3>
-    <h3 class="col-span-5">{{ title }}</h3>
+    <h3 class="col-start-1 text-[#9790A0]">
+      {{ year }}
+    </h3>
+    <h3 class="col-span-5">
+      {{ title }}
+    </h3>
     <div class="flex lg:justify-end col-span-2">
       <NuxtLink
+        v-if="isComingSoon === 'false'"
         :href="linkTo"
-        :class="{ 'text-[#9790A0] hover:text-[#332C3D]': linkText }"
+        class="hover:text-[#9790A0] text-[#332C3D]"
       >
         {{ linkText }}
       </NuxtLink>
+      <h6 v-if="isComingSoon === 'true'" class="text-[#9790A0]">
+        {{ linkText }}
+      </h6>
     </div>
   </div>
 </template>
--- components/Projects.vue
+++ components/Projects.vue
@@ -9,30 +9,35 @@
         title="Morf Health"
         linkText="View"
         linkTo="/morf-health"
+        isComingSoon="false"
       />
       <Project
         year="2023"
         title="First Republic Business Banking"
         linkText="Coming soon"
         linkTo="/first-republic"
+        isComingSoon="true"
       />
       <Project
         year="2022"
         title="Parsley Health Insights"
-        linkText="View"
+        linkText="Coming soon"
         linkTo="https://www.figma.com/proto/mu4MnZiUuasdEJDIIu3LGw/Portfolio?page-id=165%3A4101&type=design&node-id=178-11&viewport=686%2C555%2C0.1&t=uSuB9iCPpbKcZOVn-1&scaling=min-zoom&mode=design"
+        isComingSoon="true"
       />
       <Project
         year="2022"
         title="Parsley Health At-Home Lab Kits"
         linkText="View"
         linkTo="/parsley-health/at-home-lab-kits"
+        isComingSoon="false"
       />
       <Project
         year="2021"
         title="Parsley Health Member Experience"
         linkText="Coming soon"
         linkTo="/parsley-health/member-experience"
+        isComingSoon="true"
       />
     </div>
   </div>
