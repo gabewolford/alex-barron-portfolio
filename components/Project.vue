14e65aeb5bea6d36675122f1e6b1dd2001300cc9
gabewolford <gabewolford@gmail.com>
2023-11-06 19:00:30 -0800

    adjusted vertical padding on mobile

--- components/Project.vue
+++ components/Project.vue
@@ -1,7 +1,7 @@
 <script setup>
 defineProps({
   year: {
-    type: Number,
+    type: String,
     required: true,
   },
   title: {
@@ -21,7 +21,7 @@ defineProps({
 
 <template>
   <div
-    class="grid grid-cols-2 lg:grid-cols-8 gap-4 lg:gap-8 border-t border-[#D7D4DB] py-8 lg:text-2xl"
+    class="grid grid-cols-2 lg:grid-cols-8 gap-4 lg:gap-8 border-t border-[#D7D4DB] py-4 lg:py-8 lg:text-2xl"
   >
     <h3 class="col-start-1 text-[#9790A0]">{{ year }}</h3>
     <h3 class="col-span-5">{{ title }}</h3>
