632e8278ac9b1bf6f46453d64d7364a05a36172e
gabewolford <gabewolford@gmail.com>
2023-11-06 17:08:01 -0800

    tweaked gap for mobile

--- components/Project.vue
+++ components/Project.vue
@@ -21,7 +21,7 @@ defineProps({
 
 <template>
   <div
-    class="grid grid-cols-2 lg:grid-cols-8 gap-8 border-t border-[#D7D4DB] py-8 lg:text-2xl"
+    class="grid grid-cols-2 lg:grid-cols-8 gap-4 lg:gap-8 border-t border-[#D7D4DB] py-8 lg:text-2xl"
   >
     <h3 class="col-start-1 text-[#9790A0]">{{ year }}</h3>
     <h3 class="col-span-5">{{ title }}</h3>
