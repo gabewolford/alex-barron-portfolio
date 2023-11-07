2712cfc1722ae21141c837bd04658b3c60d226de
gabewolford <gabewolford@gmail.com>
2023-11-07 09:43:09 -0800

    added hover states on project links

--- components/Project.vue
+++ components/Project.vue
@@ -25,8 +25,11 @@ defineProps({
   >
     <h3 class="col-start-1 text-[#9790A0]">{{ year }}</h3>
     <h3 class="col-span-5">{{ title }}</h3>
-    <div class="flex lg:justify-end col-span-2 text-[#9790A0] text-right">
-      <NuxtLink :href="linkTo">
+    <div class="flex lg:justify-end col-span-2">
+      <NuxtLink
+        :href="linkTo"
+        :class="{ 'text-[#9790A0] hover:text-[#332C3D]': linkText }"
+      >
         {{ linkText }}
       </NuxtLink>
     </div>
