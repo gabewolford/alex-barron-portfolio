e3bdd5a99e712228a4d2a0b7dd7dba9f2eb3a133
gabewolford <gabewolford@gmail.com>
2023-11-06 16:30:49 -0800

    updated links

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -8,12 +8,18 @@
       class="col-span-2 lg:col-start-3 lg:col-span-8 flex flex-col lg:flex-row gap-8 justify-between"
     >
       <div class="flex flex-col lg:flex-row gap-8">
-        <a to="mailto:acbarron4@gmail.com" target="_blank">Email</a>
-        <a to="https://www.linkedin.com/in/alexandrabarron/" target="_blank"
-          >LinkedIn</a
+        <NuxtLink to="mailto:acbarron4@gmail.com" target="_blank"
+          >Email</NuxtLink
         >
-        <a to="" target="_blank">Resume</a>
-        <a to="" target="_blank">My Design Studio</a>
+        <NuxtLink
+          to="https://www.linkedin.com/in/alexandrabarron/"
+          target="_blank"
+          >LinkedIn</NuxtLink
+        >
+        <NuxtLink to="" target="_blank">Resume</NuxtLink>
+        <NuxtLink to="https://smallhound.co" target="_blank"
+          >My Design Studio</NuxtLink
+        >
       </div>
     </nav>
   </footer>
