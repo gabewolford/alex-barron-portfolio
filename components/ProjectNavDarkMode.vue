42639a83b64695b6cd34a4689db7aa53b9e99355
gabewolford <gabewolford@gmail.com>
2023-11-10 15:20:15 -0800

    added dark mode for equinox projects

---
+++ components/ProjectNavDarkMode.vue
@@ -0,0 +1,78 @@
+<script>
+export default {
+  data() {
+    return {
+      isMobileMenuOpen: false,
+    };
+  },
+  watch: {
+    isMobileMenuOpen(newValue) {
+      this.toggleBodyScroll(newValue);
+    },
+  },
+  methods: {
+    toggleMobileMenu() {
+      this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    },
+    toggleBodyScroll(isOpen) {
+      if (isOpen) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "auto";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <header class="flex flex-row mx-8 lg:mx-48 py-8 lg:py-16">
+    <nav class="flex flex-row w-full justify-between items-center">
+      <NuxtLink to="/">
+        <img src="/images/logo-white.svg" alt="Logo" />
+      </NuxtLink>
+
+      <!-- Mobile menu button (hidden when menu is open) -->
+      <div v-if="!isMobileMenuOpen" class="md:hidden">
+        <button @click="toggleMobileMenu">
+          <img src="/images/menu-icon-white.svg" alt="Menu" />
+        </button>
+      </div>
+
+      <!-- Close button (hidden when menu is closed) -->
+      <button
+        v-if="isMobileMenuOpen"
+        @click="toggleMobileMenu"
+        class="md:hidden absolute top-9 right-8 z-50 text-white"
+      >
+        <img src="/images/close-icon.svg" alt="Close" />
+      </button>
+
+      <!-- Mobile navigation links (full-screen overlay) -->
+      <div
+        v-if="isMobileMenuOpen"
+        class="md:hidden fixed inset-0 bg-[#332c3d] bg-opacity-95 z-40 text-white"
+      >
+        <div
+          class="flex flex-col h-full justify-center space-y-12 items-center"
+        >
+          <!-- Close button inside the mobile menu -->
+          <NuxtLink @click.native="toggleMobileMenu" to="/">Home</NuxtLink>
+          <NuxtLink @click.native="toggleMobileMenu" to="/morf-health"
+            >Morf Health</NuxtLink
+          >
+          <NuxtLink
+            @click.native="toggleMobileMenu"
+            to="/parsley-health/health-insights"
+            >Health Insights</NuxtLink
+          >
+          <NuxtLink
+            @click.native="toggleMobileMenu"
+            to="/parsley-health/at-home-lab-kits"
+            >At-Home Lab Kits</NuxtLink
+          >
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
--- layouts/projects.vue
+++ layouts/projects.vue
@@ -1,11 +1,27 @@
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
 
 <template>
-  <ProjectNav />
-  <main class="flex flex-col mx-8 lg:mx-16 mb-[258px] lg:mb-[180px]">
-    <div class="flex flex-col">
-      <slot />
-    </div>
-  </main>
-  <ProjectFooter />
+  <div>
+    <template
+      v-if="
+        route.path === '/equinox/cycling' ||
+        route.path === '/equinox/member-experience'
+      "
+    >
+      <ProjectNavDarkMode />
+    </template>
+    <template v-else>
+      <ProjectNavLightMode />
+    </template>
+    <main class="flex flex-col mx-8 lg:mx-16 mb-[258px] lg:mb-[180px]">
+      <div class="flex flex-col">
+        <slot />
+      </div>
+    </main>
+    <ProjectFooter />
+  </div>
 </template>
