36d97556fe4f957f71db94695699cfe77c7dc2a3
gabewolford <gabewolford@gmail.com>
2023-11-20 21:21:57 -0800

    renamed

--- components/FeaturedWork.vue
+++ components/FeaturedWork.vue
@@ -2,13 +2,34 @@
 
 <template>
   <div class="flex flex-col gap-4 my-8">
-    <h4 class="lg:text-lg">Recent work</h4>
+    <h4 class="lg:text-lg">Featured work</h4>
     <div class="flex flex-col">
       <Project
+        year="2021"
+        title="Parsley Health Member Vision"
+        linkText="View"
+        linkTo="/parsley-health/member-vision"
+        isComingSoon="false"
+      />
+      <Project
         year="2023"
-        title="Morf Health"
+        title="Morf Health MVP"
         linkText="View"
         linkTo="/morf-health"
+        isComingSoon="false"
+      />
+      <Project
+        year="2022"
+        title="Health Insights"
+        linkText="View"
+        linkTo="/parsley-health/health-insights"
+        isComingSoon="false"
+      />
+      <Project
+        year="2022"
+        title="At-Home Lab Kits"
+        linkText="View"
+        linkTo="/parsley-health/at-home-lab-kits"
         isComingSoon="false"
       />
       <Project
@@ -16,27 +37,6 @@
         title="First Republic Business Banking"
         linkText="Coming soon"
         linkTo="/first-republic"
-        isComingSoon="true"
-      />
-      <Project
-        year="2022"
-        title="Parsley Health Insights"
-        linkText="View"
-        linkTo="/parsley-health/health-insights"
-        isComingSoon="false"
-      />
-      <Project
-        year="2022"
-        title="Parsley Health At-Home Lab Kits"
-        linkText="View"
-        linkTo="/parsley-health/at-home-lab-kits"
-        isComingSoon="false"
-      />
-      <Project
-        year="2021"
-        title="Parsley Health Member Experience"
-        linkText="Coming soon"
-        linkTo="/parsley-health/member-experience"
         isComingSoon="true"
       />
     </div>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-8">
-    <RecentWork />
+    <FeaturedWork />
     <ArchivedWork />
   </div>
 </template>
