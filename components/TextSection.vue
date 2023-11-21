000f75f5841a7bebe2331452111debee32dc0564
gabewolford <gabewolford@gmail.com>
2023-11-20 21:22:16 -0800

    left-aligned

--- components/TextSection.vue
+++ components/TextSection.vue
@@ -15,10 +15,10 @@ defineProps({
   <section class="flex flex-col gap-8 py-8 lg:py-16 lg:mx-32">
     <div class="col-span-full lg:col-start-3 lg:col-span-8">
       <div class="flex flex-col gap-3">
-        <h4 class="text-[#716E75] text-center">{{ heading }}</h4>
-        <p
-          class="text-base lg:text-[32px] pt-8 border-[#D7D4DB] border-t lg:leading-[45px] text-center"
-        >
+        <h4 class="text-[#716E75] lg:text-2xl lg:leading-[160%]">
+          {{ heading }}
+        </h4>
+        <p class="text-base lg:text-2xl lg:leading-[160%]">
           {{ text }}
         </p>
       </div>
