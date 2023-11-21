dd1aca2b6579cab218f1b4b085c16d65109c74be
gabewolford <gabewolford@gmail.com>
2023-11-21 12:20:20 -0800

    updated gaps

--- components/RelatedProjects.vue
+++ components/RelatedProjects.vue
@@ -30,16 +30,16 @@ defineProps({
 <template>
   <section class="flex flex-col gap-8 py-8 lg:py-16 lg:mx-32">
     <div class="col-span-full lg:col-start-3 lg:col-span-8">
-      <div class="flex flex-col gap-12">
+      <div class="flex flex-col gap-6 lg:gap-12">
         <h4 class="text-[#716E75] lg:text-2xl lg:leading-[160%]">
           Related Projects
         </h4>
-        <div class="flex flex-row justify-between">
-          <NuxtLink :to="linkToLeft" class="flex flex-col gap-4">
+        <div class="flex flex-row gap-4 lg:gap-16 justify-between">
+          <NuxtLink :to="linkToLeft" class="flex flex-col gap-2 lg:gap-4">
             <img :src="imageLeft" :alt="textRight" class="w-fit h-auto" />
             <p class="underline-on-hover lg:text-2xl w-fit">{{ textLeft }}</p>
           </NuxtLink>
-          <NuxtLink :to="linkToRight" class="flex flex-col gap-4">
+          <NuxtLink :to="linkToRight" class="flex flex-col gap-2 lg:gap-4">
             <img :src="imageRight" :alt="textRight" class="w-fit h-auto" />
             <p class="underline-on-hover lg:text-2xl w-fit">{{ textRight }}</p>
           </NuxtLink>
