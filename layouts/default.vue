3121748f6b4b74333a7a0014c8738fa193eea155
gabewolford <gabewolford@gmail.com>
2024-01-02 15:16:35 -0800

    added open graph meta tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <script setup>
 import "animate.css";
+
+useSeoMeta({
+  title: "Alex Barron",
+  ogUrl: "https://alexbarron.site",
+  ogTitle: "Alex Barron",
+  description:
+    "Hello, I'm Alex. I design products for people & solve problems. As a designer, I like to start with a problem I believe is too complex for myself to solve. I work closely with my product and engineering partners to drive a path through the ambiguity and define valuable solutions. I care deeply about creating work that elevates a user experience through thoughtful interaction and visual design.",
+  ogDescription:
+    "Hello, I'm Alex. I design products for people & solve problems. As a designer, I like to start with a problem I believe is too complex for myself to solve. I work closely with my product and engineering partners to drive a path through the ambiguity and define valuable solutions. I care deeply about creating work that elevates a user experience through thoughtful interaction and visual design.",
+  ogImage: "https://alexbarron.site/images/og-image.png",
+  twitterCard: "summary_large_image",
+  twitterImage: "https://alexbarron.site/images/og-image.png",
+  twitterTitle: "Alex Barron",
+  twitterDescription:
+    "Hello, I'm Alex. I design products for people & solve problems. As a designer, I like to start with a problem I believe is too complex for myself to solve. I work closely with my product and engineering partners to drive a path through the ambiguity and define valuable solutions. I care deeply about creating work that elevates a user experience through thoughtful interaction and visual design.",
+});
 </script>
 
 <template>
